d411231fae17492a64853d3e797f8eff431c5e5a
SALWA507 <salwaamzane30@gmail.com>
2026-04-24 16:12:38 +0100

    ajouter readme de projet

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,7 +1,25 @@
 <?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=school", "root", "");
-} catch (PDOException $e) {
-    echo " Connection failed: " . $e->getMessage();
+session_start();
+if (isset($_POST["logout"])) {
+    session_destroy();
+    header('Location: login.php');
 }
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<H1>HELLO</H1>
+    <form action="#" method="POST">
+        <input type="submit" value="deconnecion" name="logout">
+    </form>
+</body>
+</html>
